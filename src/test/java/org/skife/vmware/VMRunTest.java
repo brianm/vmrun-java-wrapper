b537719a5688905ad7d72a67343b695760fab60b
Brian McCallister <brianm@skife.org>
2012-04-04 17:06:13 -0600

    avoid starting and stopping he vm so much

--- src/test/java/org/skife/vmware/VMRunTest.java
+++ src/test/java/org/skife/vmware/VMRunTest.java
@@ -2,7 +2,9 @@ package org.skife.vmware;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -16,55 +18,55 @@ import static org.skife.vmware.MoreMatchers.fileExists;
 
 public class VMRunTest
 {
-    private VMRun vmrun;
-    private File vmx;
-    private String guestUser;
-    private String guestPass;
+    private static VMRun vmrun;
+    private static File vmx;
+    private static String guestUser;
+    private static String guestPass;
 
-    @Before
-    public void setUp() throws Exception
+    @BeforeClass
+    public static void setUp() throws Exception
     {
-        this.guestUser = System.getProperty("guest-user", "atlas");
-        this.guestPass = System.getProperty("guest-pass", "atlas");
+        guestUser = System.getProperty("guest-user", "atlas");
+        guestPass = System.getProperty("guest-pass", "atlas");
 
         File execFile = new File(System.getProperty("vmrun",
                                                     "/Applications/VMware Fusion.app/Contents/Library/vmrun"));
-        this.vmx = new File(System.getProperty("vmx",
+        vmx = new File(System.getProperty("vmx",
                                                "base-ubuntu.vmwarevm/ubuntu-12.04.vmx"));
         assumeThat(execFile, fileExists());
-        this.vmrun = VMRun.withExecutableAt(execFile);
+        vmrun = VMRun.withExecutableAt(execFile);
+        vmrun.start(vmx);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception
+    {
+        vmrun.stop(vmx);
     }
 
     @Test
     public void testStartStartAndList() throws Exception
     {
-        vmrun.start(vmx);
         assertThat(vmrun.list(), equalTo(Arrays.asList(vmx.getAbsoluteFile() )));
-        vmrun.stop(vmx);
     }
 
     @Test
     public void testRunScriptInGuestWithSuccess() throws Exception
     {
-        vmrun.start(vmx);
         int exit = vmrun.runScriptInGuest(vmx, guestUser, guestPass, new File("/bin/sh"), "ifconfig -a > /tmp/ifout");
         assertThat(exit, equalTo(0));
-        vmrun.stop(vmx);
     }
 
     @Test
     public void testRunScriptInGuestWithFailure() throws Exception
     {
-        vmrun.start(vmx);
         int exit = vmrun.runScriptInGuest(vmx, guestUser, guestPass, new File("/bin/sh"), "waffles taste good");
         assertThat(exit, equalTo(127));
-        vmrun.stop(vmx);
     }
 
     @Test
     public void testCopyFileFromGuestToHost() throws Exception
     {
-        vmrun.start(vmx);
         vmrun.runScriptInGuest(vmx, guestUser, guestPass, new File("/bin/sh"), "echo 'hello guest world' > /tmp/msg");
 
         File tmp = File.createTempFile("test", ".tmp");
@@ -75,7 +77,6 @@ public class VMRunTest
         assertThat(Files.toString(tmp, Charsets.US_ASCII), equalTo("hello guest world\n"));
 
         tmp.delete();
-        vmrun.stop(vmx);
     }
 
     @Test
@@ -87,16 +88,12 @@ public class VMRunTest
         File round_trip = File.createTempFile("vmrun-wrapper", ".tmp");
         round_trip.delete();
 
-        vmrun.start(vmx);
-
         vmrun.copyFileFromHostToGuest(vmx, guestUser, guestPass, tmp, new File("/tmp/from-host"));
-
         vmrun.copyFileFromGuestToHost(vmx, guestUser, guestPass, new File("/tmp/from-host"), round_trip);
 
         assertThat(round_trip, fileContentsEqual("hello from host!"));
 
         vmrun.runScriptInGuest(vmx, guestUser, guestPass, new File("/bin/sh"), "rm /tmp/from-host");
-        vmrun.stop(vmx);
     }
 
 }
